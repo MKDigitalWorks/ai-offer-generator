49e2634e9d3b20b87eeda0cefc529908877f961c
MKDigitalWorks <mkdigitalworks.studio@gmail.com>
2025-07-13 17:58:18 +0200

    Added logo to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import OfferForm from './components/OfferForm';
+import OfferForm from "./components/OfferForm";
 
 export default function Home() {
-  return <OfferForm />;
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img src="/logo.png" alt="Logo" width="100" height="100" />
+      <OfferForm />
+    </div>
+  );
 }
