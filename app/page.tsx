80c020665295dd87c4da04b98506613fb8ae85e8
MKDigitalWorks <mkdigitalworks.studio@gmail.com>
2025-07-13 18:05:01 +0200

    Fix: Logo path and filename

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import OfferForm from "./components/OfferForm";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <img src="/logo.png" alt="Logo" width="100" height="100" />
+    <div style={{ textAlign: "center" }}>
+      <img src="/logo.png" alt="Logo" width="120" height="120" style={{ margin: "20px auto" }} />
       <OfferForm />
     </div>
   );
